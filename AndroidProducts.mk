5e094bafe3f81f1515d0cfc8b814963236a13b03
Tushar Mahajan <mahajant99@gmail.com>
2021-04-14 16:53:18 +0530

    sweet: Havoc-Os Bringup
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/havoc_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-user \
-    lineage_sweet-userdebug \
-    lineage_sweet-eng
+    havoc_sweet-user \
+    havoc_sweet-userdebug \
+    havoc_sweet-eng
